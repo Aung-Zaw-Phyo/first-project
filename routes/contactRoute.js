3122548fe2b1e229255bc4f678a3cc91389b3252
aungzawphyo <aungzawphyo1102@gmail.com>
2023-03-04 18:31:42 +0630

    validating by Joi package

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const validator = require('./../utils/validator')
+const Joi = require('joi')
 
 const {getContacts, createContact, getContact, updateContact, deleteContact} = require('../controllers/contactController')
 const validateTokenHandler = require('./../middleware/validateTokenHandler')
 
+const contactValidateSchema = Joi.object({
+    name: Joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    email: Joi.string()
+        .min(13)
+        .required(),
+    phone: Joi.string()
+        .min(9)
+        .max(14)
+        .required()
+}) // .options({ allowUnknown: false });
+
 router.use(validateTokenHandler)
-router.route('/').get(getContacts).post(createContact)
+router.route('/').get(getContacts).post( validator.validateContact(contactValidateSchema),createContact)
 
 router.route('/:id')
     .get([ validator.validateID(), getContact])
--- utils/validator.js
+++ utils/validator.js
@@ -1,4 +1,5 @@
 const { isValidObjectId } = require("mongoose")
+const asyncHandler = require('express-async-handler')
 
 const validateID = () => {  // isValidObjectId method check whether not mongoose id format, but not check that params.id whether not exist in db.
     return (req, res, next) => {
@@ -11,4 +12,16 @@ const validateID = () => {  // isValidObjectId method check whether not mongoose
     }
 }
 
-module.exports = {validateID}
+const validateContact = (schema) => {
+    return asyncHandler((req, res, next) => {
+        const result = schema.validate(req.body)
+        if(result.error){
+            res.status(400)
+            throw new Error(result.error.details[0].message.split('"').join(""))
+        }else {
+            next()
+        }
+    })
+}
+
+module.exports = {validateID, validateContact}
